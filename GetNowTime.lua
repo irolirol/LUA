850316803c3d80cd2c23d02698c4eee64534f05a
irolirol <78060274+irolirol@users.noreply.github.com>
2021-10-30 22:41:19 +0800

    Update GetNowTime.lua

--- GetNowTime.lua
+++ GetNowTime.lua
@@ -3,3 +3,12 @@
 now=os.time()
 print(os.date("%Y%m%d%H%M",now))
 print(os.date("%Y%m%d%H%M",now+120))
+
+strSeedtime = "202110301100"
+print (strSeedtime)
+
+local returnTime = os.time({year=2021, month=10, day=31, hour=10, min=00, isdst=false})
+print (returnTime)	
+
+print(os.date("%Y%m%d%H%M",returnTime))
+print(os.date("%Y%m%d%H%M",returnTime+120))
