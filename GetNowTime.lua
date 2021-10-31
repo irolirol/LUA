767af845549ecc023a35f6e5e72de8f5028c7aa0
irolirol <78060274+irolirol@users.noreply.github.com>
2021-10-31 22:43:06 +0800

    Update GetNowTime.lua

--- GetNowTime.lua
+++ GetNowTime.lua
@@ -12,3 +12,30 @@ print (returnTime)
 
 print(os.date("%Y%m%d%H%M",returnTime))
 print(os.date("%Y%m%d%H%M",returnTime+120))
+
+--mushclient
+objSeedTime = os.time({year=2021, month=10, day=31, hour=23, min=00, isdst=false})
+strNowTime = os.date("%Y%m%d%H%M",os.time())
+strSeedTime = os.date("%Y%m%d%H%M",objSeedTime)
+strSeedTimeD = os.date("%Y%m%d%H%M",objSeedTime-300)
+strSeedTimeU = os.date("%Y%m%d%H%M",objSeedTime+300)
+
+print(strSeedTime)
+print(strSeedTimeD)
+print(strNowTime)
+print(strSeedTimeU)
+
+if tonumber(strNowTime) >= tonumber(strSeedTimeD) and tonumber(strNowTime) <= tonumber(strSeedTimeU) then
+   print("it's seed time")
+   SetVariable ("VSEED_LOOP", "1")
+   EnableTriggerGroup ("START", false)
+   EnableTriggerGroup ("SEED", true)
+   EnableTimer("Force", false)
+   Execute("stop;exa")
+   Execute("goseed")
+else
+   SetVariable ("VSEED_LOOP", "0")
+   EnableTriggerGroup ("START", true)
+   EnableTriggerGroup ("SEED", false)
+   Execute("stop;exa;smile chief;exert dragon_soul")
+end
